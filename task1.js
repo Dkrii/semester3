466f1bf0eaac50c97ecfa1ab2f022bd49cd2b18d
Dkrii <160830651+Dkrii@users.noreply.github.com>
2024-09-26 13:27:29 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,62 @@
+
+//console.log(`hello`);
+console.log("jhon")
+console.log(`you can call me "santy"`);
+console.log("go way, i`m coding");
+console.log(`i am sorry\nplease forgive me`);
+
+
+//what is output display?
+let number1 = 3;
+let number2 = 9;
+let number3 = 7;
+let number4 = 6;
+let number5 = 2;
+console.log(number1 + number2 - number3 * number4 / number5);
+console.log(number1 + (number2 - number3) * number4 / number5);
+
+//3
+let conditionA = true;
+let conditionB = false;
+let conditionC = true;
+let conditionD = false;
+console.log(conditionA && conditionB || conditionC && !conditionD);
+
+//age
+let age = 9;
+let result = (age > 17) ? "adults" : (age > 10) ? "teenagers" : "childern";
+console.log(result);
+
+//num
+let numA = 176;
+let numB = 103;
+
+console.log(numA & numB);
+console.log(numA | numB);
+console.log(numA ^ numB);
+console.log(numA >> 2);
+console.log(numB >> 2);
+console.log(numA << 2);
+console.log(numB << 2);
+
+let numC = 1;
+console.log(numC << 1);
+console.log(numC << 2);
+console.log(numC << 3);
+console.log(numC << 4);
+console.log(numC << 5);
+console.log(numC << 6);
+console.log(numC << 7);
+console.log(numC << 8);
+
+//f
+let fullname = (fn,mn,ln) => {
+    return "B.Sc"
+}
+
+console.log(fullname("jhon", "dream", "myung"));
+
+
+
+
+
